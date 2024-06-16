69e2022e38605d7445b1fe1a8bffbab9db7dddb8
Rodrigo Manguinhas <rodrigommanguinhas@gmail.com>
2024-06-16 16:33:57 +0200

    testedomingo2

--- eps-api/server.js
+++ eps-api/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, '../static')));
 
 app.use((req, res, next) => {
